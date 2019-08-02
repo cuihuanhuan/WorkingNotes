7f0566f71cb6922e54c095f651b890d28cf90e49
cuihuanhuan <1696539307@qq.com>
2019-08-02 17:39:46 +0800

    Update mirror.py

--- 自动化测试脚本/mirror.py
+++ 自动化测试脚本/mirror.py
@@ -5,21 +5,22 @@ import paramiko
 import os.path
 import cx_Oracle
 from time import sleep
-import datetime
+import time
+import shutil
+import json
 
+src_ip = '192.168.80.3'
+tgt_ip = '192.168.80.4'
 
-
-src_ip = '192.168.20.1'
-tgt_ip = '192.168.19.2'
-db_info_sys = 'sqlplus system/12345@orcl'
-db_info_src = 'sqlplus huan1/12345'
-db_src = 'huan1/12345@192.168.20.1/orcl'
-db_tgt = 'huan1/12345@192.168.19.2/orcl'
-
-
+db_info_src = 'sqlplus huan1/12345@orclpdb'
+db_src = 'huan1/12345@192.168.80.3/orclpdb'
+db_tgt = 'huan1/12345@192.168.80.4/orclpdb'
+local_path='/Users/chh/Desktop/allcases_mirror/'
+uuid = '8EFA254B-1F4A-4C49-D329-9608772F2B67'
+outfile ='/Users/chh/Desktop/mirror/ret.txt'
 list_name = []
-path = "C:/allcases"
-dirs = os.listdir(path)
+
+dirs = os.listdir(local_path)
 
 for file in dirs:
     if file[-7:] == 'src.sql':
@@ -45,48 +46,55 @@ ssh_tgt.load_system_host_keys()
 ssh_tgt.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_tgt.connect(tgt_ip, 22, 'root', '12345', allow_agent=False, look_for_keys=False, timeout=5)
 
+
 for case_id in list_name:
         try:
-            #停止iawork/iatrack,重建同步用户
-            stdin, stdout, stderr = ssh_src.exec_command('cd;source .bash_profile;/root/Desktop/i2active/bin/iadebug work --stop 96227F3F-7C50-73BC-D2A7-AECC5D765746')
-            print 'cd;source .bash_profile;/root/Desktop/i2active/bin/iadebug work --stop 96227F3F-7C50-73BC-D2A7-AECC5D765746'
-            stdin, stdout, stderr = ssh_src.exec_command('cd;source .bash_profile;killall iawork;killall iatrack')
-            print 'cd;source .bash_profile;killall iawork;killall iatrack'
-            stdin, stdout, stderr = ssh_src.exec_command('cd;source .bash_profile;' + db_info_sys + '</root/Desktop/i2active/bin/create_user.sql')
-            print 'cd;source .bash_profile;' + db_info_sys + '</root/Desktop/i2active/bin/create_user.sql'
 
-
-            #停止iaback,重建同步用户
-            stdin, stdout, stderr = ssh_tgt.exec_command('cd;source .bash_profile;killall iaback')
-            print 'cd;source .bash_profile;killall iaback'
-            stdin, stdout, stderr = ssh_tgt.exec_command('cd;source .bash_profile;' + db_info_sys + '</root/Desktop/i2active/bin/create_user.sql')
-            print 'cd;source .bash_profile;' + db_info_sys + '</root/Desktop/i2active/bin/create_user.sql'
-
-
+            stdin, stdout, stderr = ssh_src.exec_command('cd;source .bash_profile;/root/Desktop/i2active/bin/iadebug work --stop '+ uuid)
+            while 1:
+                stdin, stdout, stderr = ssh_tgt.exec_command('/root/Desktop/i2active/bin/iadebug back --state '+uuid)
+                cmd_result = stdout.read(), stderr.read()
+                ret = json.loads(cmd_result[0])
+                state = ret['fullbackup']['state']
+                if state == 'pause':
+                    print 'pause'
+                    break
+                else:
+                    print 'no pause'
+                    sleep(10)
+                    continue
             #源端执行用例
-            stdin, stdout, stderr = ssh_src.exec_command('cd;source .bash_profile;' + db_info_src + ' @/allcases/' + case_id + '_src.sql')
-            print 'cd;source .bash_profile;' + db_info_src + ' @/allcases/' + case_id + '_src.sql'
+            stdin, stdout, stderr = ssh_src.exec_command('cd;source .bash_profile;' + db_info_src + ' @/allcases_mirror/' + case_id + '_src.sql')
+            stdin, stdout, stderr = ssh_src.exec_command('cd;source .bash_profile;' + db_info_src + ' @/allcases_mirror/' + case_id + '_src.sql')
 
 
-            #启动iawork/iatrack
-            stdin, stdout, stderr = ssh_src.exec_command('cd;source .bash_profile;/root/Desktop/i2active/bin/iawork;/root/Desktop/i2active/bin/iatrack')
-            print 'cd;source .bash_profile;/root/Desktop/i2active/bin/iawork;/root/Desktop/i2active/bin/iatrack'
+            print 'cd;source .bash_profile;' + db_info_src + ' @/allcases_mirror/' + case_id + '_src.sql'
 
 
-            #启动iaback
-            stdin, stdout, stderr = ssh_tgt.exec_command('cd;source .bash_profile;/root/Desktop/i2active/bin/iaback')
-            print 'cd;source .bash_profile;/root/Desktop/i2active/bin/iaback'
-            sleep(60)
+            stdin, stdout, stderr = ssh_src.exec_command('cd;source .bash_profile;/root/Desktop/i2active/bin/iadebug work --start '+uuid)
 
-            #重新同步
-            stdin, stdout, stderr = ssh_src.exec_command('cd;source .bash_profile;/root/Desktop/i2active/bin/iadebug work --start 96227F3F-7C50-73BC-D2A7-AECC5D765746')
-            print 'cd;source .bash_profile;/root/Desktop/i2active/bin/iadebug work --start 96227F3F-7C50-73BC-D2A7-AECC5D765746'
-            sleep(120)
+            while 1:
+                stdin, stdout, stderr = ssh_tgt.exec_command('/root/Desktop/i2active/bin/iadebug back --state '+uuid)
+                cmd_result = stdout.read(), stderr.read()
+                ret = json.loads(cmd_result[0])
+                state = ret['fullbackup']['state']
+                if state == 'track':
+                    print 'track'
+                    break
+                else:
+                    print 'dump'
+                    sleep(10)
+                    continue
 
-            i = datetime.datetime.now()
-            print ("fist time %s" % i)
-            filename = 'C:\\allcases\\' + case_id + '_tgt.sql'
-            print filename
+            filename = local_path + case_id + '_tgt.sql'
+
+            size = os.path.getsize(outfile)
+            if size/1024>=100:
+                shutil.move(outfile, '/users/chh/Desktop/mirror/ret-'+time.strftime("%m-%d-%H-%M", time.localtime()) +'.txt')
+            fd_ret = open(outfile, "a+")
+            print >> fd_ret, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            print >> fd_ret, filename
+
             fd = open(filename, 'r+')
             next(fd)
             next(fd)
@@ -95,23 +103,23 @@ for case_id in list_name:
                 if str == '' or str == '/':
                     continue
                 sql = line.strip()
-                print (sql)
+                print >> fd_ret, (sql)
                 cursor_src.execute(sql)
                 src_ret = cursor_src.fetchall()
-                print(src_ret)
+                print >> fd_ret, (src_ret)
                 cursor_tgt.execute(sql)
                 tgt_ret = cursor_tgt.fetchall()
-                print(tgt_ret)
+                print >> fd_ret, (tgt_ret)
                 if src_ret == tgt_ret:
-                    print 'pass'
+                    print >> fd_ret, 'pass'
                 else:
-                    print 'fail'
+                    print >> fd_ret, 'fail'
             fd.close()
-            file_src = 'C:\\allcases\\' + case_id + '_src.sql'
-            file_tgt = 'C:\\allcases\\' + case_id + '_tgt.sql'
-            os.remove(file_src)
-            os.remove(file_tgt)
-            print('del file ok')
+            fd_ret.close()
+
+            os.remove(local_path + case_id + '_src.sql')
+            os.remove(local_path + case_id + '_tgt.sql')
+
 
         except cx_Oracle.DatabaseError, msg:
             print (msg)
@@ -133,3 +141,8 @@ for case_id in list_name:
             print (msg)
             print case_id + " error! "
             continue
+
+cursor_src.close()
+conn_src.close()
+cursor_tgt.close()
+conn_tgt.close()
